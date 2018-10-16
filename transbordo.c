8c1604522830420b23d676de40b2b0f4bcfcb6b1
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2018-10-16 11:18:03 -0300

    inicializar

---
+++ transbordo.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <nSystem.h>
+
+nMonitor m;
+int *enPargua;
+
+void inicializar(int p) {
+	enPargua = malloc(sizeof(int) * p);
+	for (int i = 0; i < p; ++i)	{
+		enPargua[i] = TRUE;
+	}
+	m = nMakeMonitor();
+}
+
+void finalizar() {
+
+}
+
+void transbordoAChacao(int v) {
+
+}
+
+void transbordoAPargua(int v) {
+
+}
