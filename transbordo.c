3f5a0919062df0aeff3062ac0fa3891210d36f58
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2018-10-16 15:59:00 -0300

    comentarios

--- transbordo.c
+++ transbordo.c
@@ -3,11 +3,11 @@
 #include <nSystem.h>
 #include "transbordo.h"
 
-int *enPargua;
-int *enChacao;
-int buff_size;
-int barcosEnPargua, barcosEnChacao;
-int nextEmptyPargua, nextFullPargua, nextEmptyChacao, nextFullChacao;
+int *enPargua; // arreglo de barcos en pargua
+int *enChacao; // arreglo de barcos en chacao
+int buff_size; // tamaño de los arreglos
+int barcosEnPargua, barcosEnChacao; // cantidad de barcos en cada lado
+int nextEmptyPargua, nextFullPargua, nextEmptyChacao, nextFullChacao; // punteros para recorrer los arreglos
 nMonitor m;
 
 void inicializar(int p) {
@@ -33,12 +33,12 @@ void finalizar() {
 	nDestroyMonitor(m);
 }
 
+// obtener un transbordador en Pargua
 int getPargua() {
 	int x;
 	nEnter(m);
-	while (barcosEnPargua == 0) {
+	while (barcosEnPargua == 0)
 		nWait(m);
-	}
 	x = enPargua[nextFullPargua];
 	nextFullPargua = (nextFullPargua + 1) % buff_size;
 	barcosEnPargua--;
@@ -47,6 +47,7 @@ int getPargua() {
 	return x;
 }
 
+// estacionar un transbordador en Pargua
 void putPargua(int x) {
 	nEnter(m);
 	while (barcosEnPargua == buff_size)
@@ -58,12 +59,12 @@ void putPargua(int x) {
 	nExit(m);
 }
 
+// obtener un transbordador en Chacao
 int getChacao() {
 	int x;
 	nEnter(m);
-	while (barcosEnChacao == 0) {
+	while (barcosEnChacao == 0)
 		nWait(m);
-	}
 	x = enChacao[nextFullChacao];
 	nextFullChacao = (nextFullChacao + 1) % buff_size;
 	barcosEnChacao--;
@@ -72,6 +73,7 @@ int getChacao() {
 	return x;
 }
 
+// estacionar un transbordador en Chacao
 void putChacao(int x) {
 	nEnter(m);
 	while (barcosEnChacao == buff_size)
@@ -84,16 +86,16 @@ void putChacao(int x) {
 }
 
 void transbordoAChacao(int v) {
-	if (barcosEnChacao == buff_size)
-		transbordoAPargua(-1);
+	if (barcosEnChacao == buff_size) // si estan todos los barcos en Chacao
+		transbordoAPargua(-1); // pido un transbordador
 	int transbordador = getPargua();
 	haciaChacao(transbordador, v);
 	putChacao(transbordador);
 }
 
 void transbordoAPargua(int v) {
-	if (barcosEnPargua == buff_size)
-		transbordoAChacao(-1);
+	if (barcosEnPargua == buff_size) // si estan todos los barcos en Pargua
+		transbordoAChacao(-1); // pido un transbordador
 	int transbordador = getChacao();
 	haciaPargua(transbordador, v);
 	putPargua(transbordador);
